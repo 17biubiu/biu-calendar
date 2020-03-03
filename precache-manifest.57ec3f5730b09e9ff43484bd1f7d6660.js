44af9004608d7191d9c611fcf7b1698d9dcd79b3
lishuai <lishuai@zuoyebang.com>
2020-03-03 19:02:55 +0800

    Updates

--- precache-manifest.57ec3f5730b09e9ff43484bd1f7d6660.js
+++ precache-manifest.57ec3f5730b09e9ff43484bd1f7d6660.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d249e6ead064a0545e582215d9505817",
+    "revision": "672764b82affd38e4cb6e9a0aa9929e4",
     "url": "/biu-calendar/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/biu-calendar/static/css/2.03d7f783.chunk.css"
   },
   {
-    "revision": "4e2f7201d5c612d9e563",
-    "url": "/biu-calendar/static/css/main.aaf19e81.chunk.css"
+    "revision": "c3fe99406934bd8e95ef",
+    "url": "/biu-calendar/static/css/main.ec34b789.chunk.css"
   },
   {
     "revision": "3c3d3fc8d02e384a3c8d",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/biu-calendar/static/js/2.b2de8497.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4e2f7201d5c612d9e563",
-    "url": "/biu-calendar/static/js/main.c400988b.chunk.js"
+    "revision": "c3fe99406934bd8e95ef",
+    "url": "/biu-calendar/static/js/main.360c0a55.chunk.js"
   },
   {
     "revision": "ee8accc152d2592b4532",
